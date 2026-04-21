7b96cbacb827e85226fbdaa4a12776f50f20555b
Rakshit <yadavrakshit13@gmail.com>
2026-04-21 20:17:41 +0530

    Fix config for Next.js 16 architecture

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint: {
-    // This tells Vercel to ignore ESLint warnings (like unused variables)
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     // This tells Vercel to ignore strict type checks (like the word "any")
     ignoreBuildErrors: true,
